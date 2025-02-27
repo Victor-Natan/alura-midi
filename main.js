17d1e97e533e1dc691b2214a59cf2ebd44c62414
Victor-Natan <victornatan280@gmail.com>
2025-02-26 23:58:03 -0300

    Foi utilizado uma versão lógica do código para torná-lo mais limpo e dinâmico. Foi incluído texto dinâmico, função anônima, e uma estrutura de repetição.

--- main.js
+++ main.js
@@ -1,52 +1,21 @@
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 }
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play();
+let contador = 0;
+
+while(contador < listaDeTeclas.length){
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function(){
+        tocaSom(idAudio);
+    }
+
+    contador++;
 }
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-
-function tocaSomTim(){
-    document.querySelector('#som_tecla_tim').play();
-}
-document.querySelector('.tecla_tim').onclick = tocaSomTim;
-
-
-function tocaSomPuff(){
-    document.querySelector('#som_tecla_puff').play();
-}
-document.querySelector('.tecla_puff').onclick = tocaSomPuff;
-
-
-function tocaSomSplash(){
-    document.querySelector('#som_tecla_splash').play();
-}
-document.querySelector('.tecla_splash').onclick = tocaSomSplash;
-
-
-function tocaSomToim(){
-    document.querySelector('#som_tecla_toim').play();
-}
-document.querySelector('.tecla_toim').onclick = tocaSomToim;
-
-
-function tocaSomPsh(){
-    document.querySelector('#som_tecla_psh').play();
-}
-document.querySelector('.tecla_psh').onclick = tocaSomPsh;
-
-
-function tocaSomTic(){
-    document.querySelector('#som_tecla_tic').play();
-}
-document.querySelector('.tecla_tic').onclick = tocaSomTic;
-
-
-function tocaSomTom(){
-    document.querySelector('#som_tecla_tom').play();
-}
-document.querySelector('.tecla_tom').onclick = tocaSomTom;
